4070f3d139ed068756d190f46a041c8282db34b2
chandra Prakash <engg.chandraprakash01@gmail.com>
2026-08-04 00:56:20 +0530

    fix: implement DTO methods explicitly

--- backend/src/main/java/com/placement/controller/dto/AICandidateMatchDTO.java
+++ backend/src/main/java/com/placement/controller/dto/AICandidateMatchDTO.java
@@ -5,13 +5,60 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class AICandidateMatchDTO {
     private Student student;
     private Integer matchScore;
     private String justification;
     private Boolean hasApplied;
     private String applicationId;
+
+    public AICandidateMatchDTO() {}
+
+    public AICandidateMatchDTO(Student student, Integer matchScore, String justification, Boolean hasApplied, String applicationId) {
+        this.student = student;
+        this.matchScore = matchScore;
+        this.justification = justification;
+        this.hasApplied = hasApplied;
+        this.applicationId = applicationId;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Integer getMatchScore() {
+        return matchScore;
+    }
+
+    public void setMatchScore(Integer matchScore) {
+        this.matchScore = matchScore;
+    }
+
+    public String getJustification() {
+        return justification;
+    }
+
+    public void setJustification(String justification) {
+        this.justification = justification;
+    }
+
+    public Boolean getHasApplied() {
+        return hasApplied;
+    }
+
+    public void setHasApplied(Boolean hasApplied) {
+        this.hasApplied = hasApplied;
+    }
+
+    public String getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(String applicationId) {
+        this.applicationId = applicationId;
+    }
 }
